2a92c972508aef73d45f3dd01f7068b512668ce5
TYrueouJeL <guerinremi.pro@gmail.com>
2024-09-23 10:44:27 +0200

    Fusion des crontroller de Law

--- src/Controller/LawController.php
+++ src/Controller/LawController.php
@@ -8,19 +8,27 @@ use App\Repository\LawRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class LawCreateController extends AbstractController
+class LawController extends AbstractController
 {
+    #[Route('/law', name: 'app_law_list')]
+    public function index(EntityManagerInterface $entityManager, LawRepository $lawRepository): Response
+    {
+
+        return $this->render('law_list/index.html.twig', [
+            'lawList' => $lawRepository->findAll(),
+        ]);
+    }
+
     #[Route('/law/create', name: 'app_law_create')]
-    public function index(LawRepository $lawRepository, Request $request, EntityManagerInterface $entityManager): Response
+    public function create(LawRepository $lawRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
         $law = new Law();
 
         //Création du formulaire
-            $form = $this->createForm(LawCreateFormType::class, $law);
+        $form = $this->createForm(LawCreateFormType::class, $law);
 
         //Traitement du formulaire
         //Récupération des données
